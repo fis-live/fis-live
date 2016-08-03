ae68011f7c519ca5e1302d23a7ffae26ca1155f0
joow <esvens@tutanota.com>
2016-08-03 13:24:55 +0200

    Updated dependencies

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+    public title: string = "Hello World!!!";
+}
---
+++ src/app/main.ts
@@ -0,0 +1,12 @@
+///<reference path="../../typings/index.d.ts"/>
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+if (0) {
+    enableProdMode();
+}
+
+bootstrap(AppComponent);
