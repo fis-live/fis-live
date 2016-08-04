978759dc8d561fd2e62c1839e4e64a119c629464
joow <esvens@tutanota.com>
2016-08-04 21:01:45 +0200

    Added dropdown component

---
+++ src/app/Dropdown/dropdown.component.ts
@@ -0,0 +1,60 @@
+import {
+    Component, EventEmitter, Input, Output, AfterViewInit, OnDestroy
+} from '@angular/core';
+import { NgFor } from '@angular/common';
+
+
+
+
+
+@Component({
+    selector: 'app-dropdown',
+    template: `
+<div [attr.id]="id" class="ui top attached primary dropdown button" tabindex="0">
+    <div class="default text">Select intermediate</div>
+    <i class="dropdown icon"></i>
+    
+    <div class="menu">
+        <div class="item" data-value="0">Start list</div>
+        <div *ngFor="let item of items" class="item" [attr.data-value]="item.id" [attr.data-text]="item.distance + ' KM'">{{ item.name }}  {{ item.distance }} KM</div>
+    </div>
+</div>`,
+    directives: [NgFor]
+})
+export class DropdownComponent implements AfterViewInit, OnDestroy {
+    @Input()
+    public items: any = [];
+
+    @Input()
+    public id: string;
+
+    @Output()
+    public selected:EventEmitter<any> = new EventEmitter();
+
+    private $el: any;
+
+    ngAfterViewInit() {
+        // viewChild is set after the view has been initialized
+        console.log(this.id);
+
+        if (typeof jQuery === "undefined") {
+            console.log("jQuery is not loaded");
+            return;
+        }
+
+        this.$el = jQuery('#' + this.id);
+        console.log(this.$el);
+
+        // this.$el.dropdown({
+        //     onChange: (value) => this.selected.emit(value)
+        // });
+        //
+        // this.$el.dropdown('refresh');
+        // this.$el.dropdown('set selected', '0');
+    }
+
+    ngOnDestroy() {
+        console.log('Destroy' + this.id);
+        // this.$el.dropdown('destroy');
+    }
+}
