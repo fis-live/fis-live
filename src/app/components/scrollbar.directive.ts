6fd2605b535ef286705a844443b34f15f8c043c8
joow <esvens@tutanota.com>
2017-02-23 10:16:45 +0100

    Don't use ps on mobile

--- src/app/components/scrollbar.directive.ts
+++ src/app/components/scrollbar.directive.ts
@@ -12,22 +12,44 @@ export class ScrollbarDirective implements OnInit, OnDestroy {
     constructor (private el: ElementRef) { }
 
     ngOnInit() {
-        ps.initialize(this.el.nativeElement);
+        if (this.getScrollbarWidth() > 0) {
+            ps.initialize(this.el.nativeElement, {
+                wheelSpeed: 2
+            });
 
-        this.observer = new MutationObserver(mutations => {
-            this.update();
-        });
-        const config = { subtree: true, attributes: false, childList: true, characterData: true };
+            this.observer = new MutationObserver(mutations => {
+                this.update();
+            });
+            const config = {subtree: true, attributes: false, childList: true, characterData: true};
 
-        this.observer.observe(this.el.nativeElement, config);
+            this.observer.observe(this.el.nativeElement, config);
+        }
     }
 
     ngOnDestroy() {
-        this.observer.disconnect();
-        ps.destroy(this.el.nativeElement);
+        if (this.observer) {
+            this.observer.disconnect();
+            ps.destroy(this.el.nativeElement);
+        }
     }
 
     public update(): void {
         ps.update(this.el.nativeElement);
+    }
+
+    public getScrollbarWidth() {
+        const e = document.createElement('div');
+        e.style.position = 'absolute';
+        e.style.top = '-9999px';
+        e.style.width = '100px';
+        e.style.height = '100px';
+        e.style.overflow = 'scroll';
+        e.style.msOverflowStyle = 'scrollbar';
+
+        document.body.appendChild(e);
+        const width = (e.offsetWidth - e.clientWidth);
+        document.body.removeChild(e);
+
+        return width;
     }
 }
