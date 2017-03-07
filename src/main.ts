5fc7ae94cd365cbd52de093a5e6a254f42495b22
joow <esvens@tutanota.com>
2017-03-07 16:43:13 +0100

    Refactor styles

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 import './app/rxjs-operators';
 
+import './assets/vendor.css';
 import './assets/main.scss';
 
 if (process.env.ENV === 'production') {
