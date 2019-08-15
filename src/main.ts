861439da8c3c167daef75940f8078862f3551ea7
joow <esvens@tutanota.com>
2019-08-15 10:44:18 +0200

    Migrate to ng cli

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import './assets/main.scss';
-import './assets/vendor.css';
-
-if (process.env.NODE_ENV === 'production') {
+if (environment.production) {
     enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
